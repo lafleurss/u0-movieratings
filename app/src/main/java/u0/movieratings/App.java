5d988b083330bae04b0fbdf5077f56535972570e
shilpa nair <shilpa.sathya@gmail.com>
2022-06-12 17:06:44 -0500

    Added average ratings method

--- app/src/main/java/u0/movieratings/App.java
+++ app/src/main/java/u0/movieratings/App.java
@@ -5,6 +5,7 @@
 package u0.movieratings;
 
 import java.util.Map;
+import java.util.HashMap;
 import java.io.IOException;
 import java.util.List; 
 
@@ -17,17 +18,42 @@ public class App {
         System.out.println(new App().getGreeting());
         RatingReader reader = new RatingReader("movie-ratings-test.csv");
         List<Rating> rating = reader.getAllRatings();
+        
+
         for(Rating value : rating){
             System.out.println("Movie Name: " + value.getMovieName());
             System.out.println("Movie Rater: " + value.getRaterName());
             System.out.println("Movie Rating: " + value.getRating());
+            //myMap.add(value.getMovieName(), value.getRating());
         }
         
-        //averageRatings(rating);
+        averageRatings(rating);
     }
 
-    //public static List<Rating> averageRatings(Map <String, Integer> myMap){
+    public static Map <String, Integer>  averageRatings(List<Rating> rating) {
         //TODO Get average ratings
-    //}
+        HashMap<String, Integer> myMap = new HashMap<String, Integer>();
+        HashMap<String, Integer> averageMap = new HashMap<String, Integer>();
+        
+
+        for(Rating value: rating){ //Totalling up the ratings
+            int total = 0;
+            if(!myMap.containsKey(value.getMovieName())){
+                myMap.put(value.movieName, value.movieRating);
+            }
+            else{                
+                myMap.put(value.movieName, myMap.get(value.movieName) + value.movieRating);
+            }
+            //System.out.println(myMap.toString());
+        }    
+        	
+        for (String s : myMap.keySet()) {
+            // process each key in turn 
+            averageMap.put(s, myMap.get(s)/myMap.size());           
+        } 
+
+        System.out.println(averageMap.toString());
+        return averageMap;       
+    }
 
 }
